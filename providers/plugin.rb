1323b71948e2b814ac660c0eea95a839890b78d4
Miah Johnson <miah@chia-pet.org>
2013-08-12 20:18:01 -0700

    use the plugin command bundled with elasticsearch and allow multiple installation formats.

--- providers/plugin.rb
+++ providers/plugin.rb
@@ -1,69 +1,73 @@
+#es_test/0.90.2/bin$ ./plugin
+#Usage:
+#    -u, --url     [plugin location]   : Set exact URL to download the plugin from
+#    -i, --install [plugin name]       : Downloads and installs listed plugins [*]
+#    -r, --remove  [plugin name]       : Removes listed plugins
+#    -l, --list                        : List installed plugins
+#    -v, --verbose                     : Prints verbose messages
+#    -h, --help                        : Prints this help message
+#
+#    [*] Plugin name could be:
+#    elasticsearch/plugin/version for official elasticsearch plugins (download from download.elasticsearch.org)
+#    groupId/artifactId/version   for community plugins (download from maven central or oss sonatype)
+#    username/repository          for site plugins (download from github master)
 
 def initialize(new_resource, run_context)
   @new_resource = new_resource
   @run_context = run_context
   @instance = lookup_instance(@new_resource.instance, @run_context)
-  @source_file_res = set_source_file_resource
-  @extract_res = set_extract_resource
+  @version = @new_resource.install_options[:version]
+  @install_options = @new_resource.install_options
+  @plugin = @new_resource.plugin
+  @plugin_res = set_plugin_resource
 end
 
 action :create do
+  manage_plugin_install('install', :run)
 end
 
 action :destroy do
+  manage_plugin_install('remove', :run)
 end
 
 private
 
-def set_source_file_resource
-  Chef::Resource::RemoteFile.new(file_name, @run_context)
-end
-
-def set_extract_resource
+def set_plugin_resource
   Chef::Resource::Execute.new(source_file, @run_context)
 end
 
-def manage_extract_file(action)
-  @extract_res.user 'root'
-  @extract_res.path %w(/bin /sbin /usr/bin /usr/sbin)
-  @extract_res.command tar_command
-  @extract_res.creates instance_binary
-  @extract_res.returns 0
-  @extract_res.timeout 180
-  @extract_res.run_action(action)
+def manage_plugin_install(inst_action, run_action)
+  @plugin_res.user 'root'
+  @plugin_res.path %w(/bin /sbin /usr/bin /usr/sbin)
+  @plugin_res.command plugin_manage_command(inst_action)
+  @plugin_res.creates instance_binary
+  @plugin_res.returns 0
+  @plugin_res.timeout 180
+  @plugin_res.run_action(run_action)
 end
 
-def manage_source_file(action)
-  @source_file_res.path source_file
-  @source_file_res.source remote_file_location
-  @source_file_res.user @user
-  @source_file_res.group @group
-  @source_file_res.mode 00644
-  @source_file_res.run_action(action)
+def instance_installation_dir
+  ::File.join('', @instance.destination_dir, version)
 end
 
-def source_file
-  ::File.join('', Chef::Config[:file_cache_path], file_name)
+def plugin_command
+  ::File.join('', instance_installation_dir, 'bin', 'plugin')
 end
 
-def unzip_command
-  "unzip #{ source_file }"
+def plugin_manage_command(action)
+  "#{ plugin_command } --#{ action } #{ plugin_install_name }"
 end
 
-def instance_binary
-  ::File.join('', instance_installation_dir, 'bin', 'elasticsearch')
-end
+def plugin_install_name
+  options = @install_options
 
-def remote_file_location
-  "#{ url }/#{ file_name }"
-end
-
-def url
-  @new_resource.install_options[:url]
-end
-
-def file_name
-  "elasticsearch-#{ version }.tar.gz"
+  if options.has_key?[:groupId] && options.has_key?[:artifactId]
+    "#{ options[:groupId] }/#{ options[:artifactId] }/#{ version }"
+  elsif options.has_key?[:username] && options.has_key?[:repository]
+    "#{ options[:username] }/#{ options[:repository] }"
+  else
+    "elasticsearch/#{ @plugin }/#{ options[:version] }"
+  end
 end
 
 def lookup_resource(type, name, run_context)
