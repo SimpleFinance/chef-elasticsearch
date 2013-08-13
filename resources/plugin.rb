d3e1f63c84df89d4230ee9eacc4fa2f6be3b0284
Miah Johnson <miah@chia-pet.org>
2013-08-12 20:57:45 -0700

    drop the comment as its already out of date.

--- resources/plugin.rb
+++ resources/plugin.rb
@@ -5,8 +5,4 @@ default_action :create
 attribute :instance, kind_of: String, required: true
 attribute :plugin, kind_of: String, name_attribute: true
 attribute :plugin_timeout, kind_of: Fixnum, default: 300
-#
-# TODO
-# Should probably callback and merge user supplied with default
-# to allow selective overrides of hash options.
 attribute :install_options, kind_of: Hash
