0b14d3e8cda21c44ae82cc8784c812fe6fe4254c
Miah Johnson <miah@chia-pet.org>
2013-08-14 00:34:23 -0700

    allow user to switch install types. this is a important attribute, so moving it out of the hash.

--- resources/plugin.rb
+++ resources/plugin.rb
@@ -6,3 +6,4 @@ attribute :instance, kind_of: String, required: true
 attribute :plugin, kind_of: String, name_attribute: true
 attribute :plugin_timeout, kind_of: Fixnum, default: 300
 attribute :install_options, kind_of: Hash
+attribute :install_type, kind_of: String, equal_to: %w(manual plugin)
