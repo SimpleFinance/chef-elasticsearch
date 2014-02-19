3c94876a68d612e1a7aff001d3f6c6dd6801c2a8
Miah Johnson <miah@chia-pet.org>
2014-02-19 15:47:39 -0800

    these are heavyweight now.

--- resources/plugin.rb
+++
@@ -1,9 +0,0 @@
-
-actions :install, :remove
-default_action :install
-
-attribute :instance, kind_of: String, required: true
-attribute :plugin, kind_of: String, name_attribute: true
-attribute :plugin_timeout, kind_of: Fixnum, default: 300
-attribute :install_options, kind_of: Hash
-attribute :install_type, kind_of: String, equal_to: %w(manual plugin)
